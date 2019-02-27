363323afea69e424f78e390ef1291bdc71c8f183
micallefN <nicolas.micallef@hotmail.com>
2019-02-27 14:45:35 +0100

    mise en place routes basiques

--- app/Http/Controllers/BasicController.php
+++ app/Http/Controllers/BasicController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class BasicController extends Controller
 {
-    //
+    public function index(){
+        return view('basic/home');
+    }
+
+    public function contact(){
+        return view('basic/contact');
+    }
+
+    public function delivery(){
+        return view('basic/delivery');
+    }
 }
