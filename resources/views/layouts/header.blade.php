a5b2f00946125ae71d7984e4e559dca3e9969046
micallefN <nicolas.micallef@hotmail.com>
2019-02-28 14:07:23 +0100

    ajout navbar et footer

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,2 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/">Notre Site</a>
+    <button class="navbar-toggler mr-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="/produits">Nos produits</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/contact">Contact</a>
+            </li>
+        </ul>
+    </div>
+</nav>
--- resources/views/products/product.blade.php
+++ resources/views/products/product.blade.php
@@ -1,43 +1,38 @@
-<html>
+@extends('layouts/layout')
 
-<head>
+@section('title')
+    nos produits
+@endsection
 
-    <title>Nos Produits </title>
+@section('content')
+    <h1>Nos produits</h1>
 
+    <p>L’Empire byzantin ou Empire romain d'Orient désigne la civilisation apparue vers le IVe siècle dans la partie
+        orientale de l'Empire romain,
+        au moment où celui-ci se divise progressivement en deux.
 
-</head>
-<body>
-<h1>Nos produits</h1>
+        L’Empire byzantin se caractérise par sa longévité.
+        Il puise ses origines dans la fondation même de Rome, et la datation de ses débuts change selon les critères choisis
+        par chaque historien.
+        La fondation de Constantinople, sa capitale, par Constantin Ier en 330, autant que la division d’un Empire romain de
+        plus en plus difficile à gouverner et qui devient définitive en 395, sont parfois citées.
+        Quoi qu’il en soit, plus dynamique qu’un monde romain occidental brisé par les invasions barbares, l’Empire d’Orient
+        s’affirme progressivement comme une construction politique originale. Indubitablement romain, cet Empire est aussi
+        chrétien et de langue principalement grecque. À la frontière entre l’Orient et l’Occident, mêlant des éléments
+        provenant directement de l’Antiquité avec des aspects innovants dans un Moyen Âge parfois décrit comme grec, il
+        devient le siège d’une culture originale qui déborde bien au-delà de ses frontières, lesquelles sont constamment
+        assaillies par des peuples nouveaux. Tenant d’un universalisme romain, il parvient à s’étendre sous Justinien
+        (empereur de 527 à 565), retrouvant une partie des antiques frontières impériales, avant de connaître une profonde
+        rétractation. C’est à partir du VIIe siècle que de profonds bouleversements frappent l’Empire byzantin. Contraint de
+        s’adapter à un monde nouveau dans lequel son autorité universelle est contestée, il rénove ses structures et
+        parvient, au terme d’une crise iconoclaste, à connaître une nouvelle vague d’expansion qui atteint son apogée sous
+        Basile II (qui règne de 976 à 1025). Les guerres civiles autant que l’apparition de nouvelles menaces forcent
+        l'Empire à se transformer à nouveau sous l'impulsion des Comnènes avant d’être disloqué par la quatrième croisade
+        lorsque les croisés s'emparent de Constantinople en 1204. S’il renaît en 1261, c’est sous une forme affaiblie qui ne
+        peut résister aux envahisseurs ottomans et à la concurrence économique des républiques italiennes (Gênes et Venise).
+        La chute de Constantinople en 1453 marque sa fin. </p>
 
-<p>L’Empire byzantin ou Empire romain d'Orient désigne la civilisation apparue vers le IVe siècle dans la partie
-    orientale de l'Empire romain,
-    au moment où celui-ci se divise progressivement en deux.
-
-    L’Empire byzantin se caractérise par sa longévité.
-    Il puise ses origines dans la fondation même de Rome, et la datation de ses débuts change selon les critères choisis
-    par chaque historien.
-    La fondation de Constantinople, sa capitale, par Constantin Ier en 330, autant que la division d’un Empire romain de
-    plus en plus difficile à gouverner et qui devient définitive en 395, sont parfois citées.
-    Quoi qu’il en soit, plus dynamique qu’un monde romain occidental brisé par les invasions barbares, l’Empire d’Orient
-    s’affirme progressivement comme une construction politique originale. Indubitablement romain, cet Empire est aussi
-    chrétien et de langue principalement grecque. À la frontière entre l’Orient et l’Occident, mêlant des éléments
-    provenant directement de l’Antiquité avec des aspects innovants dans un Moyen Âge parfois décrit comme grec, il
-    devient le siège d’une culture originale qui déborde bien au-delà de ses frontières, lesquelles sont constamment
-    assaillies par des peuples nouveaux. Tenant d’un universalisme romain, il parvient à s’étendre sous Justinien
-    (empereur de 527 à 565), retrouvant une partie des antiques frontières impériales, avant de connaître une profonde
-    rétractation. C’est à partir du VIIe siècle que de profonds bouleversements frappent l’Empire byzantin. Contraint de
-    s’adapter à un monde nouveau dans lequel son autorité universelle est contestée, il rénove ses structures et
-    parvient, au terme d’une crise iconoclaste, à connaître une nouvelle vague d’expansion qui atteint son apogée sous
-    Basile II (qui règne de 976 à 1025). Les guerres civiles autant que l’apparition de nouvelles menaces forcent
-    l'Empire à se transformer à nouveau sous l'impulsion des Comnènes avant d’être disloqué par la quatrième croisade
-    lorsque les croisés s'emparent de Constantinople en 1204. S’il renaît en 1261, c’est sous une forme affaiblie qui ne
-    peut résister aux envahisseurs ottomans et à la concurrence économique des républiques italiennes (Gênes et Venise).
-    La chute de Constantinople en 1453 marque sa fin. </p>
-
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, ab accusamus ea incidunt itaque laborum minus nulla
-    perferendis possimus provident quaerat ullam voluptas! Aliquam, error neque obcaecati quisquam suscipit
-    voluptates?</p>
-
-</body>
-
-</html>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, ab accusamus ea incidunt itaque laborum minus nulla
+        perferendis possimus provident quaerat ullam voluptas! Aliquam, error neque obcaecati quisquam suscipit
+        voluptates?</p>
+@endsection
