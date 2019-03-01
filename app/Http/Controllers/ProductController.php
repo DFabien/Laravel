f33ba37cc55deee684945d8470f0a41e39698ad0
marlene <marlene.pinel@hotmail.fr>
2019-03-01 14:11:21 +0100

    page panier + css

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,24 +11,39 @@ class ProductController extends Controller
         $blonde = array(
             'name' => 'Grimbergen Blonde',
             'price' => 5.25,
+            'description' => 'RONDE ET ÉQUILIBRÉE. Une bière blonde légère aux notes 
+                                de fruits mûrs, parfaitement limpide, à la mousse dense 
+                                couleur crème.',
             'photo' => 'images/blonde_verre.png'
         );
 
         $blanche = array (
             'name' => 'Grimbergen Blanche',
             'price' => 5.49,
+            'description' => 'FRAÎCHE ET ÉQUILIBRÉE. Une bière blanche légèrement trouble aux 
+                                arômes frais d’agrumes, de clous de girofle, de coriandre et de 
+                                bergamote, le tout agrémenté d’une mousse crémeuse.',
             'photo' => 'images/blanche_verre.png'
         );
         
         $ambree = array (
             'name' => 'Grimbergen Ambrée',
             'price' => 6.17,
+            'description' => 'RICHE ET MALTÉE. Une bière brune haute fermentation aux notes 
+                                douces-amères, rehaussées d’un soupçon de caramel et de 
+                                pruneaux, avec des sortes de houblon et de malt fermentées 
+                                deux fois.',
             'photo' => 'images/ambree_verre.png'
         );
 
         $rouge = array (
             'name' => 'Grimbergen Rouge',
             'price' => 5.69,
+            'description' => 'RONDE ET FRUITÉE. Une bière fruitée, douce-amère, forte de la 
+                                tradition belge; elle allie les arômes de fruits rouges et de 
+                                fraises aux notes du clou de girofle, tout en lui conférant un 
+                                léger caractère épicé sous une robe rouge aux reflets rubis, une 
+                                mousse généreuse et un parfum intense.',
             'photo' => 'images/rouge_verre.png'
         );
 
--- resources/views/products/product.blade.php
+++ resources/views/products/product.blade.php
@@ -1,15 +1,34 @@
 @extends('layouts/layout')
 
 @section('title')
-    nos produits
+    Nos produits
 @endsection
 
 @section('content')
-    <h1>Nos produits</h1>
+    <h1 class="titre-principal">Nos produits</h1>
 
-    @foreach ($liste as $produit)
-    <p>{{$produit ["name"]}}</p>
-    <p>{{$produit ["price"]}}</p>
-    <img src="{{asset ($produit["photo"])}}" >
-    @endforeach
+<div class="container-fluide">
+    <h2 class="titre-secondaire">Bières Grimbergen</h2>
+        @foreach ($liste as $produit)
+        <div class="row">
+                <div class="col-lg-4"><img src="{{asset ($produit["photo"])}}"></div>
+                <div class="col-lg-8">{{$produit ["name"]}} </br>
+                <p>{{$produit ["price"]}}€ <p></br>
+                <p>{{$produit ["description"]}}</p>
+            </div>
+        </div>
+        @endforeach
+
+    <h2 class="titre-secondaire">Bières Du Monde</h2>
+        @foreach ($liste as $produit)
+        <div class="row">
+            <div class="col-lg-4"><img src="{{asset ($produit["photo"])}}"></div>
+            <div class="col-lg-8">{{$produit ["name"]}} </br>
+            <p>{{$produit ["price"]}}€ <p></br>
+            <p>{{$produit ["description"]}}</p>
+            </div>
+            </div>
+        @endforeach
+</div>
+
 @endsection
