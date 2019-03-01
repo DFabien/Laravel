157ebbdfe4524174d84df2bbabae4f079def5f44
marlene <marlene.pinel@hotmail.fr>
2019-03-01 14:21:23 +0100

    modif layout

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,12 +7,9 @@
     <title>@yield('title')</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<<<<<<< HEAD
-    <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
-=======
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}">
->>>>>>> dd600722606b8f1b361b238b48a8cf8ac4e4af8a
+    <link rel="stylesheet" type="text/css" href="{{asset('css/style.css') }}">
+
 
 
 </head>
