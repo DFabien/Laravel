174081603aeaa725e3aed3afee8aca2e3018029d
marlene <marlene.pinel@hotmail.fr>
2019-03-01 16:34:14 +0100

    ajouter panier bouton

--- resources/views/products/product.blade.php
+++ resources/views/products/product.blade.php
@@ -9,15 +9,18 @@
 
 <div class="container-fluide">
     <h2 class="titre-secondaire">Bières Grimbergen</h2>
-        @foreach ($liste as $produit)
-        <div class="row">
+        <form method="post" action="basket.blade.php">
+            @foreach ($liste as $produit)
+            <div class="row">
                 <div class="col-lg-4"><img src="{{asset ($produit["photo"])}}"></div>
                 <div class="col-lg-8">{{$produit ["name"]}} </br>
                 <p>{{$produit ["price"]}}€ <p></br>
                 <p>{{$produit ["description"]}}</p>
+                <input type="submit" class="btn" value="Ajouter au panier">            
+                </div> 
             </div>
-        </div>
-        @endforeach
+            @endforeach
+        </form>
 
     <h2 class="titre-secondaire">Bières Du Monde</h2>
         @foreach ($liste as $produit)
@@ -26,8 +29,9 @@
             <div class="col-lg-8">{{$produit ["name"]}} </br>
             <p>{{$produit ["price"]}}€ <p></br>
             <p>{{$produit ["description"]}}</p>
+            <button type="submit" class="btn">Ajouter au panier</button>
             </div>
-            </div>
+        </div>
         @endforeach
 </div>
 
