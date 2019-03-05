4be54988b0924a1c824c3e5e5b606840a59fffaf
micallefN <nicolas.micallef@hotmail.com>
2019-03-05 09:29:22 +0100

    raw sql queries

--- resources/views/products/description.blade.php
+++ resources/views/products/description.blade.php
@@ -5,9 +5,10 @@
 @endsection
 
 @section('content')
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus consequatur ducimus explicabo, fugit iusto
-        labore laborum maiores modi nesciunt obcaecati officiis quis quod sapiente similique sint tenetur unde vel?
-    </p>
-    @endsection
+
+    <p>{{ $product->name }}</p>
+    <p>{{ $product->price / 100 }} €</p>
+    <p>{{ $product->descr }}</p>
+
+@endsection
 
--- resources/views/products/product.blade.php
+++ resources/views/products/product.blade.php
@@ -5,34 +5,15 @@
 @endsection
 
 @section('content')
-    <h1 class="titre-principal">Nos produits</h1>
+    <div class="container">
+        <h1 class="titre-principal">Nos produits</h1>
 
-<div class="container-fluide">
-    <h2 class="titre-secondaire">Bières Grimbergen</h2>
-        <form method="post" action="basket.blade.php">
-            @foreach ($liste as $produit)
-            <div class="row">
-                <div class="col-lg-4"><img src="{{asset ($produit["photo"])}}"></div>
-                <div class="col-lg-8">{{$produit ["name"]}} </br>
-                <p>{{$produit ["price"]}}€ <p></br>
-                <p>{{$produit ["description"]}}</p>
-                <input type="submit" class="btn" value="Ajouter au panier">            
-                </div> 
-            </div>
-            @endforeach
-        </form>
-
-    <h2 class="titre-secondaire">Bières Du Monde</h2>
-        @foreach ($liste as $produit)
-        <div class="row">
-            <div class="col-lg-4"><img src="{{asset ($produit["photo"])}}"></div>
-            <div class="col-lg-8">{{$produit ["name"]}} </br>
-            <p>{{$produit ["price"]}}€ <p></br>
-            <p>{{$produit ["description"]}}</p>
-            <button type="submit" class="btn">Ajouter au panier</button>
-            </div>
-        </div>
+        @foreach($liste as $product)
+            <p>{{ $product->name }}</p>
+            <p>{{ $product->price / 100 }} €</p>
+            <p><a href="{{route('product', ['id' => $product->id ])}}">Detail</a></p>
         @endforeach
-</div>
+
+    </div>
 
 @endsection
