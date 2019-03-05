e5790e7ba72123a88ebf2b08532b04b80a4bf131
micallefN <nicolas.micallef@hotmail.com>
2019-03-05 13:15:23 +0100

    ajout du model

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,19 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Product;
 
 class ProductController extends Controller
 {
     public function showAll()
     {
-        $products = DB::select('select * from product');
+
+        // $products = DB::select('select * from product');
+
+        $products = Product::All();
 
         return view('products.product', ['liste'=>$products]);
     }
     public function show($id){
 
-        $product = DB::select('select * from product where id = :id', ['id' => $id]);
+       // $product = DB::select('select * from product where id = :id', ['id' => $id]);
 
-        return view('products.description' , ['product' => $product[0]]);
+        $product = Product::find($id);
+
+        return view('products.description' , ['product' => $product]);
     }
 }
