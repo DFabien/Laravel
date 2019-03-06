5d082a5e7360504acf7f8fe7fb65a10a2c4b380b
sebastien-haruto <sebastien.zaidi@le-campus-numerique.fr>
2019-03-06 11:52:46 +0100

    Une page contenant la ​listes des produits​ de la BDD

--- resources/views/products/product.blade.php
+++ resources/views/products/product.blade.php
@@ -9,30 +9,19 @@
 
 <div class="container-fluide">
     <h2 class="titre-secondaire">Bières Grimbergen</h2>
-        <form method="post" action="basket.blade.php">
-            @foreach ($liste as $produit)
+
+            @foreach ($products as $produit)
             <div class="row">
-                <div class="col-lg-4"><img src="{{asset ($produit["photo"])}}"></div>
-                <div class="col-lg-8">{{$produit ["name"]}} </br>
-                <p>{{$produit ["price"]}}€ <p></br>
-                <p>{{$produit ["description"]}}</p>
+                <div class="col-lg-4">></div>
+                <div class="col-lg-8">{{$produit->nom}} </br>
+                <p>{{$produit->price}}€ <p></br>
+                <p>{{$produit ->description}}</p>
                 <input type="submit" class="btn" value="Ajouter au panier">            
                 </div> 
             </div>
             @endforeach
-        </form>
 
-    <h2 class="titre-secondaire">Bières Du Monde</h2>
-        @foreach ($liste as $produit)
-        <div class="row">
-            <div class="col-lg-4"><img src="{{asset ($produit["photo"])}}"></div>
-            <div class="col-lg-8">{{$produit ["name"]}} </br>
-            <p>{{$produit ["price"]}}€ <p></br>
-            <p>{{$produit ["description"]}}</p>
-            <button type="submit" class="btn">Ajouter au panier</button>
-            </div>
-        </div>
-        @endforeach
+
 </div>
 
 @endsection
