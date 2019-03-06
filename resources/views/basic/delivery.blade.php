24cc192b8dc25c8700ab87a6a965139aff77b0b2
marlene <marlene.pinel@hotmail.fr>
2019-03-06 17:11:18 +0100

    page livraison

--- resources/views/basic/delivery.blade.php
+++ resources/views/basic/delivery.blade.php
@@ -10,33 +10,6 @@ Livraison
 <div class="container">
     <div class="emballage">
         <div class="row">
-            <div class="col">
-                <img src="https://img.saveur-biere.com/themes/sb3/images/anti_breakage_packages1.png" alt="anti_breakage_packages1">
-            </div>
-            <div class="col">
-                <h2 class="subtitel">Emballage des bouteilles</h2>
-                <p>Nos bières sont placées dans des cartons renforcés séparément, pour éviter tout risque de casse Nos cartons peuvent contenir jusqu'à 24 bouteilles, placées à la verticale pour plus de sécurité.</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <img src="https://img.saveur-biere.com/themes/sb3/images/anti_breakage_packages2.png" alt="anti_breakage_packages1">
-            </div>
-            <div class="col">
-                <h2 class="subtitel">Emballage des fûts</h2>
-                <p>Tous nos cartons sont optimisés de façon à parfaitement accueillir nos produits et peuvent contenir jusque 3 fûts. Nous utilisons un système de broyage écologique de recyclage de cartons pour protéger nos fûts des chocs.</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <img src="https://img.saveur-biere.com/themes/sb3/images/anti_breakage_packages3.png" alt="anti_breakage_packages1">
-            </div>
-            <div class="col">
-                <h2 class="subtitel">Emballage des verres</h2>
-                <p>Nos verres sont doublement protégés ! En plus d'un carton, ils sont enveloppés de bulles plastiques renforcées grâce à notre machine à coussin d'air à l'intérieur d'un autre carton.</p>
-            </div>
-        </div>
-    </div>
     <div class="livraison">
         <h2>À domicile</h2>
         <div class="row">
@@ -45,8 +18,8 @@ Livraison
                 <h3>livraison en 48h</h3>
                 <p>Livraison à domicile ou au bureau, avec livraison interactive par email et sms pour agir sur la livraison la veille de réception.</p>
             </div>
-            <div class="col"
             <div class="col">
+            
                 <div><img src="https://img.saveur-biere.com/home/cms/page_cms/2018_02_06_15_25_25mini_logo_chronopost.png" alt="logo"></div>
                 <h3>livraison en 24h, avant 13h</h3>
                 <p>Livraison express à domicile ou au bureau, avant 13h, avec livraison interactive par email et sms pour agir sur la livraison la veille de réception. </p>
@@ -66,16 +39,11 @@ Livraison
                 <p>La livraison express avant 13h dans l'un des nombreux points retraits Chronopost, et suivi par email et SMS de l'acheminement du colis.</p>
             </div>
         </div>
-        <h2>À la palette</h2>
-        <div>
-            <div class="logo_image" style="margin-top:10px"><img src="https://img.saveur-biere.com/home/cms/page_cms/2018_02_06_15_25_31mini_logo_geodis.png" alt="logo"></div>
-            <h3 class="subtitel">livraison en 72h à votre porte</h3>
-            <p>Tarifs en fonction de la commande (+ frais de 2nd passage si vous êtes absent lors de la livraison convenue par rendez-vous) pour tous les produits lourds et fragiles type tireuse à bière professionnelle, cuves de brassage...</p>
-
-        </div>
+    
     </div>
 
 </div>
+    </div></div>
 
 
 
