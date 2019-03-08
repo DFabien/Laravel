5b9f7faa3bce69df0dc530ce34491854261796c8
sebastien-haruto <sebastien.zaidi@le-campus-numerique.fr>
2019-03-08 10:49:30 +0100

    Une page contenant la liste des produits  ​triées par nom​ en utilisant l‘ORM

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\TheProducts;
 
 class ProductController extends Controller
 {
     public function showAll()
     {
-        $produits = DB::select('select * from products');
-       // dd($produits); // dump and die afficher ce que contient la variable en parametre et arrete l'execution du code
+       $produits = TheProducts::all()->sortBy("nom");
             return view('products.product',['products' =>$produits]);
     }
     public function show($id){
@@ -20,3 +20,19 @@ class ProductController extends Controller
         return view('products.description',['products' =>$produits[0]]);
     }
 }
+
+/*class ProductController extends Controller
+{
+    public function showAll()
+    {
+        $produits = DB::select('select * from products');
+        // dd($produits); // dump and die afficher ce que contient la variable en parametre et arrete l'execution du code
+        return view('products.product',['products' =>$produits]);
+    }
+    public function show($id){
+        $produits = DB::select("select nom,description,price from products WHERE id_products = $id");
+        // double cote accepte variables php mais pas simple cote
+
+        return view('products.description',['products' =>$produits[0]]);
+    }
+}*/
---
+++ app/TheProducts.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TheProducts extends Model
+{
+    protected $table = 'products';  //nom de la table concerné par cette classe
+}
