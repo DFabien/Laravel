f3f0bac409555b43dcabd8b2f88a2d01c8311216
marlene <marlene.pinel@hotmail.fr>
2019-03-19 16:03:44 +0100

    modification productcontroller suite aux relations et changement de nom du fichier des models

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,20 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Products_Model;
+use App\Article;
 
 class ProductController extends Controller
 {
     public function showAll()
     {
         //$produits = Products_Model::where('id_cat', 1)->get(); //fonction qui permet de selectionner la catégorie 1 dans mon tableau qui correspond à mon model
-        $produits = Products_Model::all();
+        $produits = Article::all();
         return view('products/product',['liste'=>$produits]);
     }
 
 
     public function show($id){
-        $details= Products_Model::find($id);   //fonction qui permet d'afficher uniquement l'article voulu.
+        $details= Article::find($id);   //fonction qui permet d'afficher uniquement l'article voulu.
 
         return view('products/description', ['produit'=>$details]);
     }
