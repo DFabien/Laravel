d4e6956635daf6bd0940153cf9db9e92d7849bee
micallefN <nicolas.micallef@hotmail.com>
2019-03-19 09:20:37 +0100

    maj footer et finalisation homepage

--- resources/views/basic/home.blade.php
+++ resources/views/basic/home.blade.php
@@ -1,24 +1,71 @@
 @extends('layouts/layout')
 
 @section('title')
-Accueil
+    Accueil
 @endsection
 
 @section('content')
 
-    <div class="container">
-        <div>
-            pohgfkhpgf
-        </div>
-        <div>
-            pohgfkhpgf
-        </div>
-        <div>
-            pohgfkhpgf
-        </div>
-        <div>
-            pohgfkhpgf
+    <div class="topHomePage">
+        <div class="container">
+            <div class="row">
+                <div class="homeTopContent col-lg-6 col-sm-12">
+                    <h3>SquirrelBeers</h3>
+
+                    <h1>Craft Beer ShopCave à bières artisanales</h1>
+
+                    <p>Notre souhait, vous faire découvrir le meilleur des bières artisanales françaises et étrangères.
+                        Venez découvrir nos 400 références en boutique et notre sélection de bouteilles et canettes en
+                        ligne. Livraison entre 24 et 72h ou retrait à la cave.
+                    </p>
+                    <a href="{{ route('products') }}">Voir les produits</a>
+                </div>
+            </div>
         </div>
     </div>
+    <div class="container">
+        <div class="brasseries">
+            <h4>Nos brasseries</h4>
+            <div class="row">
+                <div class="col">
+                    <div>
+                        <img src="{{asset('images/brasseries/lostabbey.png')}}" alt="lost abbey">
+                        <h5>The Lost Abbey</h5>
+                    </div>
+                </div>
+                <div class="col">
+                    <div>
+                        <img src="{{asset('images/brasseries/portbrewing.png')}}" alt="lost abbey">
+                        <h5>Port Brewing</h5>
+                    </div>
+                </div>
+                <div class="col">
+                    <div>
+                        <img src="{{asset('images/brasseries/partizan.png')}}" alt="lost abbey">
+                        <h5>Partizan</h5>
+                    </div>
+                </div>
+                <div class="col">
+                    <div>
+                        <img src="{{asset('images/brasseries/oldnation.png')}}" alt="lost abbey">
+                        <h5>Old Nation</h5>
+                    </div>
+                </div>
+                <div class="col">
+                    <div>
+                        <img src="{{asset('images/brasseries/jesterking.png')}}" alt="lost abbey">
+                        <h5>Jester King</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+
 
 @endsection
+
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,15 +1,27 @@
-<nav class="navbar navbar-light bg-light footerNav">
+<div class="container-fluid footerNav">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h2>A propos</h2>
+                <p>SquirrelBeers</p>
+                <p>Vous faire découvrir le meilleur des bières artisanales françaises et étrangères en ligne ou à la cave</p>
+            </div>
+            <div class="col">
+                <img src="{{ asset('images/logoSquirrel.png') }}">
+                <p>
+                    7 Rue Hoche<br/>
+                    38000 Grenoble<br/>
+                    Mardi au vendredi : 13:00–19:30<br/>
+                    Samedi : 11:00–19:30<br/>
+                </p>
+            </div>
+            <div class="col">
+                <h2>Nous suivre</h2>
+                <p>Contact</p>
+                <p>Mentions légales</p>
+                <p>Conditions générales de vente</p>
+            </div>
+        </div>
+    </div>
+</div>
 
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="{{route('products')}}">Nos produits</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{route('contact')}}">Contact</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{route('delivery')}}">Livraison</a>
-            </li>
-        </ul>
-
-</nav>
