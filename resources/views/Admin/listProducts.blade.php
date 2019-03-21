9aeaf0aac7cd3e95c6cc6abad121dff003f8ca90
marlene <marlene.pinel@hotmail.fr>
2019-03-21 09:29:45 +0100

    reparation des boutons (close, no) de la fenetre modal

--- resources/views/Admin/listProducts.blade.php
+++ resources/views/Admin/listProducts.blade.php
@@ -5,15 +5,15 @@
 @endsection
 
 @section('content')
-    
+
 <h1>PRODUCTS LIST</h1>
 
 <p>
-<a type="button" href="{{route('adminProductAdd')}}"><i class="fas fa-cart-plus"></i> 
-  Add a product</a>
-</p>
-
-<table class="table">
+  <a type="button" href="{{route('adminProductAdd')}}"><i class="fas fa-cart-plus"></i> 
+    Add a product</a>
+  </p>
+  
+  <table class="table">
     <thead class="thead-dark">
       <tr>
         <th scope="col">id</th>
@@ -37,37 +37,37 @@
           @if($produit->discount)
           {{$produit->discount->id}} - {{$produit->discount->name}}
           @else()NULL
-        @endif
+          @endif
         </td>
         <td><a type="button" href="{{route('adminEdit', $produit->id)}}"><i class="far fa-edit"></i></a></td>
         
         <!-- Button trigger modal -->
         <td><button type="button" data-toggle="modal" data-target="{{'#modal' . $produit->id}}"><i class="far fa-trash-alt"></i></button></td>
-
+        
         <!-- Modal -->
         <div class="modal fade" id="{{'modal' . $produit->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabelCenterTitle" aria-hidden="true">
           <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
               <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLongTitle">
-                    Do you confirm the deletion ?</h5>
-                <button type="button" class="close" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-footer">
-                <a type="button" class="btn btn-secondary">No</a>
-                <form action={{route('adminProductDelete', $produit->id)}} method="post"> 
+                  Do you confirm the deletion ?</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-footer">
+                  <a type="button" data-dismiss="modal" class="btn btn-secondary">No</a>
+                  <form action={{route('adminProductDelete', $produit->id)}} method="post"> 
                     @method('delete')
                     @csrf
-                  <input type="submit" class="btn btn-primary" value="Yes">
-              </form>
+                    <input type="submit" class="btn btn-primary" value="Yes">
+                  </form>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </tr>
-      @endforeach </tbody>
-    </table>
-    
-    @endsection
+        </tr>
+        @endforeach </tbody>
+      </table>
+      
+      @endsection
