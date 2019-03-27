93c30942ed29e5812626398090e40c0163db8baf
micallefN <nicolas.micallef@hotmail.com>
2019-03-27 15:46:20 +0100

    ptit netoyage de adminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -14,56 +14,10 @@ class AdminController extends Controller
 
         return view('Admin/home', ['orders' => $orders, 'articles' => $articles]);
     }
-    public function showCatAll(){
-        return view('Admin/category');
-    }
-    public function addCat(){
-        return view('Admin/addCategory');
-    }
-
-    public function updateCat($id){
-        return view('Admin/updateCategory');
-    }
-
-    public function deleteCat($id){
-        return view('Admin/category');
-    }
-
-    public function showProd($id){
-        return view('Admin/showProduct');
-    }
-
-    public function showDiscAll(){
-        return view('Admin/discount');
-    }
-
-    public function addDiscount(){
-        return view('Admin/addDiscount');
-    }
-
-    public function updateDiscount(){
-        return view('Admin/updateDiscount');
-    }
-
-    public function deleteDiscount($id){
-        return view('Admin/discount');
-    }
 
     public function showDelivAll(){
         $deliveries = config('delivery');
         return view('Admin.delivery' , ['deliveries' => $deliveries]);
-    }
-
-    public function showUsersAll(){
-        return view('Admin.users');
-    }
-
-    public function orderUser($id){
-        return view('Admin.orderUser');
-    }
-
-    public function orderDetailUser ($id){
-        return view('Admin.orderDetailUser');
     }
 
 }
