fd2894c46b100c6f005d8ce3daaf75dd751ee4ed
micallefN <nicolas.micallef@hotmail.com>
2019-03-27 13:43:39 +0100

    maj page produit

--- resources/views/products/description.blade.php
+++ resources/views/products/description.blade.php
@@ -5,24 +5,23 @@
 @endsection
 
 @section('content')
-    <div class="container-fluid">
-        <h1 class="titre-principal">Description</h1>
-
-        <div class="row">
-            <div class="col-lg-6 produit">
+    <div class="container">
+        <div class="row productDetail">
+            <div class="col-lg-6 produit center">
                 <img class="image" src="{{asset("/images/bieres/".$produit->photo)}}" alt="Photo de "/>
             </div>
-            <div class="col-lg-6 desc">
-                <h3>{{$produit->name}}</h3><br>
-                <p>{{$produit->description}}</p><br>
-                <p>{{$produit->price/100}}€</p><br>
+            <div class="col-lg-6 descritption">
+                <h5>{{$produit->category->category}}</h5>
+                <h3><strong>{{$produit->name}}</strong></h3>
+                <p>{{$produit->description}}</p>
+                <h4>{{$produit->price/100}} €</h4>
 
                 <form action="{{route('basketAdd', $produit->id)}}" method="post">
                     @method('PUT')
                     @csrf
 
-                    <input type="number" name="quantity" min="0" max="{{$produit->stock}}" value="1">
-                    <input type="submit" value="Ajouter au panier">
+                    <input type="number" name="quantity" min="0" max="{{$produit->stock}}" value="1" class="inputProduct">
+                    <input type="submit" value="Ajouter au panier" class="submitProduct">
                 </form>
             </div>
         </div>
