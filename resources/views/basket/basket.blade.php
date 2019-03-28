cc5f7bee5c9d1b16c225bdb879c128174572eae4
micallefN <nicolas.micallef@hotmail.com>
2019-03-28 16:45:48 +0100

    maj css panier et suppression panier apres validation commande

--- resources/views/basket/basket.blade.php
+++ resources/views/basket/basket.blade.php
@@ -5,36 +5,52 @@
 @endsection
 
 @section('content')
-    <div class="container-fluid">
-        <h1 class="titre-principal">Panier</h1>
+    <div class="topPageBasket topBasketPage">
+        <div class="container">
+            <div class="row">
+                <div class="TopContent col-lg-6 col-sm-12">
+                    <h3>SquirrelBeers</h3>
+                    <h1>MON PANIER</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
         <div class="row">
-
             <table class="table panier">
                 <thead>
                 <tr>
-                    <th scope="col">Images</th>
-                    <th scope="col">Nom</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Prix/U</th>
-                    <th scope="col">quantity</th>
-                    <th scope="col">Prix Total</th>
                     <th :scope="col"></th>
-
+                    <th :scope="col"></th>
+                    <th scope="col">Produit</th>
+                    <th scope="col">Prix</th>
+                    <th scope="col">quantité</th>
+                    <th scope="col">Total</th>
                 </tr>
                 </thead>
                 <tbody>
                 @foreach($produits as $produit)
-
                     <tr>
+                        <td>
+                            <form action="{{route('basketDelete', $produit['produit']->id)}}" method="post">
+                                @method('DELETE')
+                                @csrf
+                                <button type="button" class="far fa-times-circle" data-toggle="modal"
+                                        data-target="{{'#modal' . $produit['produit']->id }}"></button>
+
+                                @include ('layouts.modalDelete')
+                            </form>
+
+                        </td>
                         <td>
                             <img class="images" src="{{ asset("/images/bieres/".$produit['produit']->photo)}}"
                                  alt="Photo de {{$produit['produit']->name}}"/>
                         </td>
-                        <td>{{$produit['produit']->name}}</td>
-                        <td>{{$produit['produit']->description}}</td>
+                        <td>
+                            {{$produit['produit']->name}}
+                        </td>
                         <td>{{$produit['produit']->price/100}}€</td>
                         <td>
-
                             <form action="{{route('basketUpdate', $produit['produit']->id)}}" method="post">
                                 @method('PUT')
                                 @csrf
@@ -48,24 +64,22 @@
                         </td>
 
                         <td>{{($produit['produit']->price/100) * ($produit['quantity'])}}€</td>
-                        <td>
-                            <form action="{{route('basketDelete', $produit['produit']->id)}}" method="post">
-                                @method('DELETE')
-                                @csrf
 
-
-                                <button type="button" class="fas fa-trash" data-toggle="modal"
-                                        data-target="{{'#modal' . $produit['produit']->id }}"></button>
-
-                                @include ('layouts.modalDelete')
-                            </form>
-
-                        </td>
                     </tr>
                 @endforeach
 
                 <tr>
-                    <td></td>
+                    <td>
+                        @if(!empty($produits))
+                            <form action="{{route('basketErase')}}" method="post">
+                                @method('DELETE')
+                                @csrf
+                                    <button type="button" class="far fa-times-circle" data-toggle="modal" data-target="#exampleModal1"></button>
+                                @include ('layouts/modalErase')
+
+                            </form>
+                        @endif
+                    </td>
                     <td></td>
                     <td></td>
                     <td></td>
@@ -74,22 +88,13 @@
                 </tr>
                 </tbody>
             </table>
-            <form action="{{route('basketErase')}}" method="post">
-                @method('DELETE')
-                @csrf
-
-                <p>
-                    <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#exampleModal1">Vider le Panier</button></p>
-                    @include ('layouts/modalErase')
-               </p>
-            </form>
-
 
         </div>
-
-        <form action="{{ route('basketSave') }}" method="POST">
-            @csrf
-            <input type="submit" value="Valider la commande" class="btn-ico btn-ico-green">
-        </form>
+        @if(!empty($produits))
+            <form action="{{ route('basketSave') }}" method="POST" class="right">
+                @csrf
+                <input type="submit" value="Valider la commande" class="basketValidation">
+            </form>
+        @endif
     </div>
 @endsection
